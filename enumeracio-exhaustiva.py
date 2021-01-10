0995d31fb7946e273f5d68c33705c585e1c8fb0d
kevin jimenez <kivito2009@hotmail.com>
2021-01-10 18:55:41 -0500

    busqueda exhaustida

---
+++ enumeracio-exhaustiva.py
@@ -0,0 +1,10 @@
+objetivo = int(input('Escoge un entero: '))
+respuesta = 0
+while respuesta**2 < objetivo:
+    print(respuesta)
+    respuesta += 1
+
+if respuesta**2 == objetivo:
+    print(f'la raiz cuadrada de {objetivo} es {respuesta}')
+else:
+    print(f'el {objetivo} no tiene una raiz cuadrada exacta')
